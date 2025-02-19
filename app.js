121e8ca5ad4cb3591b20218ce1db6d7299caae79
Ryu-02 <ymq02110192@gmail.com>
2025-02-19 11:05:11 +0900

    로그인 에러 수정

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
+
 const router = require("./routes/index");
 const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
@@ -40,7 +41,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production", // production 환경에서만 secure 쿠키 사용
+      secure: process.env.NODE_ENV === "development", // production 환경에서만 secure 쿠키 사용
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24, // 24시간
     },
