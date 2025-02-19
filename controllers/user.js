e74ebb95af820eadaaa91dfd8b84059be1ebf431
Ryu-02 <ymq02110192@gmail.com>
2025-02-19 14:26:27 +0900

    서버 오류 잡기

--- controllers/user.js
+++ controllers/user.js
@@ -44,8 +44,20 @@ const login = async (req, res) => {
       name: response.name,
     };
 
-    return sendResponse(res, 200, true, "로그인 성공", {
-      user: { email: response.email },
+    req.session.save((err) => {
+      console.log("세션 보내기 성공");
+      if (err) {
+        console.error("Session save error:", err);
+        return sendResponse(
+          res,
+          500,
+          false,
+          "세션 저장 중 오류가 발생했습니다."
+        );
+      }
+      return sendResponse(res, 200, true, "로그인 성공", {
+        user: { email: response.email },
+      });
     });
   } catch (err) {
     console.error("Login error:", err);
